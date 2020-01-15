e5fe2dca07d097aa77fcb4895a16c3564bc390ea
Joël Meiller <>
2020-01-15 19:49:35 +0100

    add templates as static list to page

--- imports/ui/pages/Template.js
+++ imports/ui/pages/Template.js
@@ -20,6 +20,33 @@ import { ButtonBar } from '../components/molecules/forms/ButtonBar'
 import { CHANGE_TEMPLATE } from '../graphql/mutations'
 import { PAGE } from '../graphql/queries'
 
+const Templates = [
+  {
+    templateId: '1111',
+    title: 'Template for Location Manager',
+    image:
+      'https://res.cloudinary.com/optune-me/image/upload/c_scale,w_1500/v1557913264/optune/app/solutions/solution_location_manager.jpg',
+  },
+  {
+    templateId: '2222',
+    title: 'Template for Artist',
+    image:
+      'https://res.cloudinary.com/optune-me/image/upload/c_scale,w_1500/v1557913263/optune/app/solutions/solution_artist.jpg',
+  },
+  {
+    templateId: '3333',
+    title: 'Template for Booking Agent',
+    image:
+      'https://res.cloudinary.com/optune-me/image/upload/c_scale,w_1500/v1557913263/optune/app/solutions/solution_booking_agent.jpg',
+  },
+  {
+    templateId: '4444',
+    title: 'Template for Promoter',
+    image:
+      'https://res.cloudinary.com/optune-me/image/upload/c_scale,w_1500/v1557913266/optune/app/solutions/solution_promoter.jpg',
+  },
+]
+
 const Template = ({ page }) => {
   const { t } = useTranslation()
 
